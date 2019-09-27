84f1ec5f89151ea6cc6d23d953e23be8d6be5e63
小池 <875267425@qq.com>
2019-09-27 09:37:34 +0800

    优化 validate验证

--- public/static/src/lib/utils/validate.js
+++ public/static/src/lib/utils/validate.js
@@ -1,89 +1,19 @@
 import regexp from './regexp.js'
-
-export default Object.assign({
-    //判断字符是否为空的方法
+let validate = {
     isEmpty:function(obj){
         if(typeof obj == "undefined" || obj == null || obj == ""){
             return true;
         }else{
             return false;
         }
-    },
-    // 用户名验证
-    isUsername:function(val){
-        if (val && regexp.usenameRegExp.test(val)) {
+    }
+};
+for (let key in regexp){
+    validate['is'+key.replace(key[0],key[0].toUpperCase())] = function(val){
+        if (val && regexp[key].test(val)) {
             return true;
         }
         return false;
-    },
-    // 密码验证
-    isPassword:function(val){
-        if (val && regexp.passwordRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 邮箱验证
-    isEmail:function(val){
-        if (val && regexp.emailRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 手机验证
-    isPhone:function(val){
-        if (val && regexp.phoneRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 手机验证
-    isTel:function(val){
-        if (val && regexp.telRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 身份证验证
-    isIdcard:function(val){
-        if (val && regexp.idCardRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // ip验证
-    isIp:function(val){
-        if (val && regexp.ipRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 中文验证
-    isChinese:function(val){
-        if (val && regexp.chineseRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 日期验证
-    isDate:function(val){
-        if (val && regexp.dateRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // url验证
-    isUrl:function(val){
-        if (val && regexp.dateRegExp.test(val)) {
-            return true;
-        }
-        return false;
-    },
-    // 非零正整数验证
-    isInteger:function(val){
-        if (val && regexp.integer.test(val)) {
-            return true;
-        }
-        return false;
-    },
-},regexp);
+    }
+}
+export default Object.assign(regexp,validate);
