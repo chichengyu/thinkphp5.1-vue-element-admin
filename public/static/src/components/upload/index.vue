b491d283bfd15f6e52456c99fb9f3b2d504a31e5
xiaochi <875267425@qq.com>
2019-10-10 17:05:09 +0800

    优化 dialog\preview\upload组件

--- public/static/src/components/upload/index.vue
+++ public/static/src/components/upload/index.vue
@@ -79,7 +79,7 @@ export default {
         return {
             dialogImageUrl: '',
             visible: false,
-            headers:{token:this.$store.getters.userInfo.token||'1231321'},
+            headers:{token:this.$store.getters.userInfo.token||''},
             // fileList:[
             //     {name: 'food.jpg', url:'http://p1.meituan.net/wedding/b8a49d1f1b3eb3d4e9eefcddb67bd16b166188.jpg%40watermark%3D1%26%26r%3D1%26p%3D9%26x%3D2%26y%3D2%26relative%3D1%26o%3D20'}
             // ],
