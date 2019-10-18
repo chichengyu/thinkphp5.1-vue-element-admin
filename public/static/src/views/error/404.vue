fcaf124b00879bea61954dcbb2c8636240aea04a
xiaochi <875267425@qq.com>
2019-10-18 10:55:20 +0800

    更新 404.vue

--- public/static/src/views/error/404.vue
+++ public/static/src/views/error/404.vue
@@ -11,13 +11,13 @@ export default {
 	}
 }
 </script>
-<style scoped>
+<style lang="css" scoped>
 .notfound{
 	width: 100%;
 	height: 100%;
 	overflow: hidden;
 }
-.notfound img{
+.notfound > img{
 	width: 100%;
 	height: 100%;
 }
