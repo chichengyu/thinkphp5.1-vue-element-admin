e68f164addd8e98fefc18e8bab5b9933a32ec1f4
xiaochi <875267425@qq.com>
2019-10-18 17:11:04 +0800

    新增 popover组件

---
+++ public/static/src/views/example/popover/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="popover" style="display:inline-block;margin-right:6px;vertical-align:middle;">
+        <component-popover :width="300"
+               :text="'触发此内容将显示popover'"
+               :size="'按钮样式'"
+               :placement="'right-top'"
+               :tag="'span'"
+               :trigger="'hover'"
+               :tagStyle="{color:'red'}"
+               :params="'额外参数'"
+               @ok="handleOk"
+               @cancel="handleCancel">
+        </component-popover>
+
+        <hr />
+
+        <!-- 扩展内容 -->
+        <component-popover :isSlot="true" :tag="'span'" :trigger="'hover'" :tagStyle="{color:'red'}">
+            <template slot="content">
+                <span>aaaaaaaaaaa</span>
+            </template>
+        </component-popover>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Popover',
+    methods:{
+        handleCancel:function () {
+            console.log('额外参数',params);
+        },
+        handleOk:function () {
+            console.log('额外参数',params);
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
